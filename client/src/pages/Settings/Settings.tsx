ea9f220ff914f59be1c8e2221056f6f0e3e3f0d5
Bryan <bryandang10@gmail.com>
2024-03-15 01:16:25 -0700

    finish navbar

--- client/src/pages/Settings/Settings.tsx
+++ client/src/pages/Settings/Settings.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import { Text, Flex } from "@mantine/core";
 import { NavbarSimple } from "../../components/Navbar/NavbarSimple";
 
 function SettingsPage() {
+  const [active, setActive] = useState('Settings');
+
   return (
     <Flex>
-      <NavbarSimple />
+      <NavbarSimple active={active} setActive={setActive} />
       <Flex>
         <Text>SETTINGS CONTENT SHOULD GO IN HERE</Text>
       </Flex>
--- client/src/pages/Statistics/Statistics.tsx
+++ client/src/pages/Statistics/Statistics.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import { Text, Flex } from "@mantine/core";
 import { NavbarSimple } from "../../components/Navbar/NavbarSimple";
 
 function StatisticsPage() {
+  const [active, setActive ] = useState('Statistics')
+
   return (
     <Flex>
-      <NavbarSimple />
+      <NavbarSimple active={active} setActive={setActive} />
       <Flex>
         <Text>STATISTICS CONTENT SHOULD GO IN HERE</Text>
       </Flex>
