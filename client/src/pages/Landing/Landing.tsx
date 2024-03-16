d4b2c6164099eff2f00679d6464df22ab7eabc97
yenu <35312312+yenuGH@users.noreply.github.com>
2024-03-16 10:13:27 -0700

    format landing.tsx, remove useState from app.tsx

--- client/src/pages/Landing/Landing.tsx
+++ client/src/pages/Landing/Landing.tsx
@@ -1,77 +1,70 @@
 // TODO: If the user comes here but we find that they are logged in, redirect to "/timer" page
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { Center, Box, Flex, Text, Title, Button } from "@mantine/core";
 
 function LandingPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function handleSignIn() {
-        // TODO: this will navigate to Auth0's login page (what stokely said)
-        navigate('/timer');
-    }
+  function handleSignIn() {
+    // TODO: this will navigate to Auth0's login page (what stokely said)
+    navigate("/timer");
+  }
 
-    function signInButton(): JSX.Element {
-        return (
-            <>
-                <Button
-                    onClick={handleSignIn}
-                    variant="gradient"
-                    gradient={{
-                        from: 'yellow',
-                        to: 'orange',
-                        deg: 90
-                    }}
-                >
-                    SIGN IN
-                </Button>
-            </>
-        );
-    }
-
-    function templifyTitle(): JSX.Element {
-        return (
-            <>
-                <Center>
-                    <Title
-                        order={1}
-                        size={100}
-                    >
-                        <Text
-                            inherit
-                            span
-                            fw={900}
-                            variant='gradient'
-                            gradient={{
-                                from: 'rgba(255, 157, 71, 1)',
-                                to: 'rgba(252, 210, 96, 1)',
-                                deg: 90
-                            }}
-                        >
-                            TEMPLIFY
-                        </Text>
-                    </Title>
-                </Center>
-            </>
-        );
-    }
-
+  function signInButton(): JSX.Element {
     return (
-        <>
-            <Center style={{ height: '100vh' }}>
-                <Box style={{ width: 200 }}>
-                    <Flex direction="column" gap="md">
-                        {templifyTitle()}
-                        {signInButton()}
-                    </Flex>
-                </Box>
-            </Center>
-        </>
+      <>
+        <Button
+          onClick={handleSignIn}
+          variant="gradient"
+          gradient={{
+            from: "yellow",
+            to: "orange",
+            deg: 90,
+          }}
+        >
+          SIGN IN
+        </Button>
+      </>
     );
+  }
 
+  function templifyTitle(): JSX.Element {
+    return (
+      <>
+        <Center>
+          <Title order={1} size={100}>
+            <Text
+              inherit
+              span
+              fw={900}
+              variant="gradient"
+              gradient={{
+                from: "rgba(255, 157, 71, 1)",
+                to: "rgba(252, 210, 96, 1)",
+                deg: 90,
+              }}
+            >
+              TEMPLIFY
+            </Text>
+          </Title>
+        </Center>
+      </>
+    );
+  }
 
-
-
+  return (
+    <>
+      <Center style={{ height: "100vh" }}>
+        <Box style={{ width: 200 }}>
+          <Flex direction="column" gap="md">
+            {templifyTitle()}
+            {signInButton()}
+          </Flex>
+        </Box>
+      </Center>
+    </>
+  );
 }
 
 export default LandingPage;
