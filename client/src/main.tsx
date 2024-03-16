67f0c37fbde7d83259ae6558bad1872f91fd0a80
stokely <stokelydiamantis@gmail.com>
2024-03-15 21:47:26 -0700

    resolve isLoading bug

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import "./index.css";
+import { MantineProvider } from "@mantine/core";
+import { BrowserRouter } from "react-router-dom";
+import { Auth0ProviderWithNavigate } from "./Auth0ProviderWithNavigate";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MantineProvider>
+      <BrowserRouter>
+        <Auth0ProviderWithNavigate>
+          <App />
+        </Auth0ProviderWithNavigate>
+      </BrowserRouter>
+    </MantineProvider>
+  </React.StrictMode>
+);
