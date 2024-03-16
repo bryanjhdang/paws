7db6fe0145b91d31997ae090727ae6b59aa458b2
stokely <stokelydiamantis@gmail.com>
2024-03-15 18:49:58 -0700

    update Router to use auth guards

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,65 @@
-import { useState } from 'react'
-import './App.css'
-import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/Landing/Landing';
-import TimerPage from './pages/Timer/Timer';
-import PetPage from './pages/Pet/Pet';
-import StatisticsPage from './pages/Statistics/Statistics';
-import FriendsPage from './pages/Friends/Friends';
-import ProfilePage from './pages/Profile/Profile';
-import SettingsPage from './pages/Settings/Settings';
+// import { useState } from "react";
+import "./App.css";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/Landing/Landing";
+import TimerPage from "./pages/Timer/Timer";
+import PetPage from "./pages/Pet/Pet";
+import StatisticsPage from "./pages/Statistics/Statistics";
+import FriendsPage from "./pages/Friends/Friends";
+import ProfilePage from "./pages/Profile/Profile";
+import SettingsPage from "./pages/Settings/Settings";
+import { Auth0ProviderWithNavigate } from "./Auth0ProviderWithNavigate";
+import { useAuth0 } from "@auth0/auth0-react";
+import { PageLoader } from "./components/PageLoader";
+import { AuthenticationGuard } from "./components/AuthenticationGuard";
 
-function App() {
+export const App: React.FC = () => {
+  const { isLoading } = useAuth0();
+
+  // if (isLoading) {
+  //   return (
+  //     // TODO: style
+  //     <div>
+  //       <PageLoader />
+  //     </div>
+  //   );
+  // }
+
   return (
     <MantineProvider>
       <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/timer" element={<TimerPage />} />
-          <Route path="/pet" element={<PetPage />} />
-          <Route path="/statistics" element={<StatisticsPage />} />
-          <Route path="/friends" element={<FriendsPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-        </Routes>
+        <Auth0ProviderWithNavigate>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route
+              path="/timer"
+              element={<AuthenticationGuard component={TimerPage} />}
+            />
+            <Route
+              path="/pet"
+              element={<AuthenticationGuard component={PetPage} />}
+            />
+            <Route
+              path="/statistics"
+              element={<AuthenticationGuard component={StatisticsPage} />}
+            />
+            <Route
+              path="/friends"
+              element={<AuthenticationGuard component={FriendsPage} />}
+            />
+            <Route
+              path="/profile"
+              element={<AuthenticationGuard component={ProfilePage} />}
+            />
+            <Route
+              path="/settings"
+              element={<AuthenticationGuard component={SettingsPage} />}
+            />
+          </Routes>
+        </Auth0ProviderWithNavigate>
       </Router>
     </MantineProvider>
-  )
-}
-
-export default App
+  );
+};
