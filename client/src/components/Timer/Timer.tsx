e318f88489aa43dadde15b59ac9f39198740f3a1
yenu <35312312+yenuGH@users.noreply.github.com>
2024-03-16 12:57:12 -0700

    change slider max value to 120, making it easier to map

--- client/src/components/Timer/Timer.tsx
+++ client/src/components/Timer/Timer.tsx
@@ -13,10 +13,8 @@ import {
 export function Timer(): JSX.Element {
   /* ---------------------------------- State --------------------------------- */
   const [timerSliderValue, setTimerSliderValue] = useState<number>(0);
-
   const [timerProgressTextValue, setTimerProgressTextValue] =
     useState<string>(`0:00`);
-
   const [timerProgressWheelValue, setTimerProgressWheelValue] =
     useState<number>(0);
   const [timerProgressWheelRounding, setTimerProgressWheelRounding] =
@@ -26,25 +24,29 @@ export function Timer(): JSX.Element {
   function convertSliderValueToSeconds(value: number): number {
     let seconds: number = 0;
 
-    /* 
-            slider value to minutes mapping (as slider only goes up to 100)
-            0 -> 0
-            25 -> 30 minutes
-            50 -> 60 minutes
-            75 -> 90 minutes
-            100 -> 120 minutes
-
-            therefore slider value to seconds and milliseconds mapping is:
-            0 -> 0
-            25 -> 1800 seconds -> 1800000 milliseconds
-            50 -> 3600 seconds -> 3600000 milliseconds
-            75 -> 5400 seconds -> 5400000 milliseconds
-            100 -> 7200 seconds -> 7200000 milliseconds
-        */
-    // formula to convert slider value to seconds
-    seconds = Math.floor((value / 100) * 7200);
+    // the slider supports all 120 minutes, therefore just multiply by 60
+    seconds = value * 60;
 
     return seconds;
+  }
+  function convertSecondsToProgressTextValue(seconds: number): string {
+    // we take the floor of the seconds divided by 60 to get the minutes
+    // we take the remainder of the seconds divided by 60 to get the remaining seconds
+    let minutes = Math.floor(seconds / 60);
+    let remainingSeconds = seconds % 60;
+
+    // padStart is used to ensure that the string str is at least 2 characters long.
+    // If str is less than 2 characters long, padStart will add "0"s to the start of str
+    // until it is 2 characters long. this is for seconds < 10
+    return `${minutes}:${String(remainingSeconds).padStart(2, "0")}`;
+  }
+  function convertSecondsToProgressWheelValue(seconds: number): number {
+    let value: number = 0;
+
+    // reverse the conversion
+    value = seconds / 60;
+
+    return value;
   }
 
   /* ----------------------------- Event Handlers ----------------------------- */
@@ -58,13 +60,14 @@ export function Timer(): JSX.Element {
     }
 
     let seconds = convertSliderValueToSeconds(value);
-    console.log("minutes:seconds", seconds / 60, ":", seconds % 60);
 
-    setTimerProgressTextValue(value.toString());
+    setTimerProgressTextValue(convertSecondsToProgressTextValue(seconds));
     setTimerSliderValue(value);
     setTimerProgressWheelValue(value);
   }
-  function handleTimerStartButton(): void {}
+  function handleTimerStartButton(): void {
+    console.log("Timer Started");
+  }
 
   /* ------------------------------- Components ------------------------------- */
   function timerToolTip(): JSX.Element {
@@ -126,6 +129,36 @@ export function Timer(): JSX.Element {
     );
   }
   function timerSlider(): JSX.Element {
+    const stepValue: number = 5;
+    const maxValue: number = 120;
+    const marks = [
+        { value: 0, label: "0 minutes" },
+        { value: 5, label: "" },
+        { value: 10, label: "" },
+        { value: 15, label: "" },
+        { value: 20, label: "" },
+        { value: 25, label: "" },
+        { value: 30, label: "30 minutes" },
+        { value: 35, label: "" },
+        { value: 40, label: "" },
+        { value: 45, label: "" },
+        { value: 50, label: "" },
+        { value: 55, label: "" },
+        { value: 60, label: "60 minutes" },
+        { value: 65, label: "" },
+        { value: 70, label: "" },
+        { value: 75, label: "" },
+        { value: 80, label: "" },
+        { value: 85, label: "" },
+        { value: 90, label: "90 minutes" },
+        { value: 95, label: "" },
+        { value: 100, label: "" },
+        { value: 105, label: "" },
+        { value: 110, label: "" },
+        { value: 115, label: "" },
+        { value: 120, label: "120 minutes" },
+    ];
+
     return (
       <>
         <Slider
@@ -133,14 +166,8 @@ export function Timer(): JSX.Element {
           showLabelOnHover={false}
           color={"#f1d179"}
           onChange={(value) => handleTimerSlider(value)}
-          // step={25} // this makes the slider jump in increments of 25 (each half hour)
-          marks={[
-            { value: 0, label: "0 minutes" },
-            { value: 25, label: "30 minutes" },
-            { value: 50, label: "60 minutes" },
-            { value: 75, label: "90 minutes" },
-            { value: 100, label: "120 minutes" },
-          ]}
+          max={maxValue}
+          marks={marks}
         />
       </>
     );
@@ -175,10 +202,10 @@ export function Timer(): JSX.Element {
         flex={1}
       >
         {timerProgressWheel()}
+
+        {/* TODO: transition animation to stop/pause buttons */}
         {timerSlider()}
-
         <Space h={50} />
-
         {timerStartButton()}
       </Flex>
     </>
