c2548fdf05262a6a18a347cbcdc0748b10daa40f
Bryan <bryandang10@gmail.com>
2024-03-20 00:23:11 -0700

    add models.tsx and delete node modules from root

---
+++ client/src/classes/models.tsx
@@ -0,0 +1,45 @@
+export class Pet {
+  constructor(
+    public id: string,
+    public name: string,
+    public imageUrl: string,
+  ) { }
+}
+
+export class Project {
+  constructor(
+    public id: string,
+    public hex: string,
+    public name: string,
+  ) { };
+}
+
+export class Settings {
+  constructor(
+    // Empty for now
+  ) { }
+}
+
+export class TimeEntry {
+  constructor(
+    public id: string,
+    public startTime: number,
+    public endTime: number,
+    public project: Project,
+    public name: string,
+    public earnedCoins: number,
+  ) { }
+}
+
+export class User {
+  constructor(
+    public id: string,
+    public displayName: string,
+    public pet: Pet,
+    public timeEntries: TimeEntry[],
+    public currentTimerStart: number,
+    public projects: Project[],
+    public totalCoins: number,
+    public currentTimeEntry?: TimeEntry,
+  ) { }
+}
