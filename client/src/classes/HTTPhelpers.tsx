621d4290461ab2d1022fa2bd911e380efe85a23c
unknown <bryandang10@gmail.com>
2024-03-20 15:24:30 -0700

    setup post and get requests for projects

---
+++ client/src/classes/HTTPhelpers.tsx
@@ -0,0 +1,65 @@
+import axios, { AxiosPromise, AxiosResponse } from "axios";
+import { Pet, Project, TimeEntry } from "./models";
+
+// Account
+export function getAccount(): void {
+
+}
+
+export function postAccountCreate(): void {
+
+}
+
+// Pet
+export function getPet(): void {
+
+}
+
+export function getCoins(): number {
+	// make api call
+	return 0;
+}
+
+// TimeEntry
+export function getTimeEntry(): void {
+
+}
+
+export function postTimeEntryStart(): void {
+
+}
+
+export function postTimeEntryStop(): void {
+
+}
+
+// Projects
+export async function postProject(project: Project) {
+	axios({
+		method: 'post',
+		url: `${import.meta.env.VITE_API_SERVER_URL}/timeEntry/projects`,
+		data: {
+			name: project.name,
+			hexColor: project.hex
+		}
+	})
+	.then ((response) => {
+		return response;
+	}, (error) => {
+		console.log(error);
+		throw(error);
+	});
+}
+
+export async function getProjects() {
+	axios({
+		method: 'get',
+		url: `${import.meta.env.VITE_API_SERVER_URL}/timeEntry/projects`
+	})
+	.then ((response) => {
+		return response;
+	}, (error) => {
+		console.log(error);
+		throw(error);
+	});
+}
