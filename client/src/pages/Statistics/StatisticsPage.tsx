4504d59c8e8a52c9cf2c6d9ebbaa1ed3582d35ab
unknown <bryandang10@gmail.com>
2024-03-20 20:07:43 -0700

    add json to pages

--- client/src/pages/Statistics/StatisticsPage.tsx
+++ client/src/pages/Statistics/StatisticsPage.tsx
@@ -1,15 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Text, Flex } from "@mantine/core";
 import { NavbarSimple } from "../../components/Navbar/NavbarSimple";
+import { TimeEntry } from "../../classes/models";
+import { getTimeEntry } from "../../classes/HTTPhelpers";
 
 function StatisticsPage() {
   const [active, setActive ] = useState('Statistics')
+  const [timeEntries, setTimeEntries] = useState<TimeEntry[]>([]);
+
+  useEffect(() => {
+    getTimeEntry().then(
+      (response) => {
+        setTimeEntries(response);
+      }
+    )
+  }, []);
 
   return (
     <Flex>
       <NavbarSimple active={active} setActive={setActive} />
-      <Flex>
-        <Text>STATISTICS CONTENT SHOULD GO IN HERE</Text>
+      <Flex direction={'column'}>
+        <Text>The statistics stuff is under construction. Come back after the checkpoint. Here's a JSON representation of the data.</Text>
+        <Text style={{ whiteSpace: 'pre-line' }}>{JSON.stringify(timeEntries, null, 2)}</Text>
       </Flex>
     </Flex>
   );
