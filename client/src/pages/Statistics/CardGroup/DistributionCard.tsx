3fcfb2e63dc6c512f0cde29272f55fff9325296a
yenu <35312312+yenuGH@users.noreply.github.com>
2024-03-28 16:43:22 -0700

    import echart graphs into card model

--- client/src/pages/Statistics/CardGroup/DistributionCard.tsx
+++ client/src/pages/Statistics/CardGroup/DistributionCard.tsx
@@ -1,7 +1,51 @@
-import { Card, rgba } from "@mantine/core";
-import * as echarts from "echarts";
+import { Card } from "@mantine/core";
+
+import ReactECharts from "echarts-for-react";
 
 export function DistributionCard(props: any) {
+  const chart = {
+    tooltip: {
+      trigger: "item",
+    },
+    legend: {
+      top: "5%",
+      left: "center",
+    },
+    series: [
+      {
+        name: "Access From",
+        type: "pie",
+        radius: ["40%", "70%"],
+        avoidLabelOverlap: false,
+        padAngle: 5,
+        itemStyle: {
+          borderRadius: 10,
+        },
+        label: {
+          show: false,
+          position: "center",
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: 40,
+            fontWeight: "bold",
+          },
+        },
+        labelLine: {
+          show: false,
+        },
+        data: [
+          { value: 1048, name: "Search Engine" },
+          { value: 735, name: "Direct" },
+          { value: 580, name: "Email" },
+          { value: 484, name: "Union Ads" },
+          { value: 300, name: "Video Ads" },
+        ],
+      },
+    ],
+  };
+
   return (
     <>
       <Card
@@ -10,10 +54,9 @@ export function DistributionCard(props: any) {
         bg={"rgba(241, 209, 121, 1)"}
         c={"black"}
         radius={"md"}
-        mih={"25em"}
       >
         <h2>{props.title}</h2>
-        <h3>{props.subtitle}</h3>
+        <ReactECharts option={chart} />
         <p>{props.description}</p>
       </Card>
     </>
