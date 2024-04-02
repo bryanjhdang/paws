b34de827743b3bf5499ab472ecff57ef8732f934
yenu <35312312+yenuGH@users.noreply.github.com>
2024-04-02 09:44:45 -0700

    generate new statistics on /timeEntry/generate

--- client/src/pages/GenerateStatistics/GenerateStatisticsPage.tsx
+++ client/src/pages/GenerateStatistics/GenerateStatisticsPage.tsx
@@ -2,18 +2,20 @@ import { useState } from "react";
 import { Text, Flex, Button } from "@mantine/core";
 import { NavbarSimple } from "../../components/Navbar/NavbarSimple";
 
+import { TimeEntry } from "../../classes/models";
+
 function GenerateStatisticsPage() {
   const [active, setActive] = useState('Friends');
 
   function generateStatistics() {
-    
+
   }
 
   return (
     <Flex>
       <NavbarSimple active={active} setActive={setActive} />
       <Flex>
-        <Text>Generate statistics</Text>
+        <Text>Generate statistics this page does nothing disregard</Text>
         <Button onClick={generateStatistics}>Generate</Button>
       </Flex>
     </Flex>
--- server/src/services/timeEntry.service.ts
+++ server/src/services/timeEntry.service.ts
@@ -10,61 +10,90 @@ import { resolve } from "path";
 import { rejects } from "assert";
 
 export class TimeEntryService {
-  constructor(private db: DatabaseHelper,
-    private accountService: AccountHelper) { };
+  constructor(
+    private db: DatabaseHelper,
+    private accountService: AccountHelper
+  ) {}
 
-  startEntry(user: User, description: string, projectId: string, startTime: number, endTime : number) {
+  startEntry(
+    user: User,
+    description: string,
+    projectId: string,
+    startTime: number,
+    endTime: number
+  ) {
     if (user.currentTimeEntry.earnedCoins != -1) {
-      throw Error("Unable to start an entry when user already has entry running");
+      throw Error(
+        "Unable to start an entry when user already has entry running"
+      );
     }
-    user.currentTimeEntry = new TimeEntry('', startTime, endTime, projectId, description, 0);
+    user.currentTimeEntry = new TimeEntry(
+      "",
+      startTime,
+      endTime,
+      projectId,
+      description,
+      0
+    );
     console.log("Updating user" + user);
     firestoreHelper.updateUser(user);
   }
 
-  stopEntry(user : User, endTime: number): Promise<TimeEntry> {
+  stopEntry(user: User, endTime: number): Promise<TimeEntry> {
     return new Promise<TimeEntry>((resolve, reject) => {
       if (user.currentTimeEntry.earnedCoins != -1) {
-        let newTimeEntry = new TimeEntry('', 
-            user.currentTimeEntry.startTime, 
-            endTime, 
-            user.currentTimeEntry.projectId,
-            user.currentTimeEntry.name, 0);
-        newTimeEntry.earnedCoins = (newTimeEntry.endTime - newTimeEntry.startTime) % 1000;
+        let newTimeEntry = new TimeEntry(
+          "",
+          user.currentTimeEntry.startTime,
+          endTime,
+          user.currentTimeEntry.projectId,
+          user.currentTimeEntry.name,
+          0
+        );
+        newTimeEntry.earnedCoins =
+          (newTimeEntry.endTime - newTimeEntry.startTime) % 1000;
 
         user.currentTimeEntry = new TimeEntry();
         user.currentTimeEntry.earnedCoins = -1; // todo: think about how to do this better in the future
         user.totalCoins += newTimeEntry.earnedCoins;
 
         firestoreHelper.updateUser(user);
-        firestoreHelper.createTimeEntry(user.id, newTimeEntry).then((timeEntry) => {
-          resolve(timeEntry);
-        });
+        firestoreHelper
+          .createTimeEntry(user.id, newTimeEntry)
+          .then((timeEntry) => {
+            resolve(timeEntry);
+          });
       } else {
         console.log("User currently has no running time entry");
         reject(new Error("User currently has no running time entry"));
       }
-  
     });
   }
 
-  query(user: User, start: Date, end: Date, proejctId: string, name: string) : Promise<TimeEntry[]> {
+  query(
+    user: User,
+    start: Date,
+    end: Date,
+    proejctId: string,
+    name: string
+  ): Promise<TimeEntry[]> {
     return firestoreHelper.getTimeEntries(user.id);
   }
 
-  getProjects(user: User) : Promise<Project[]> {
+  getProjects(user: User): Promise<Project[]> {
     return firestoreHelper.getProjects(user.id);
   }
 
-  createProject(user : User, name : string, hex : string) : Promise<string> {
+  createProject(user: User, name: string, hex: string): Promise<string> {
     let newProject = new Project("", hex, name);
     return firestoreHelper.createProject(user.id, newProject);
   }
+
+  generateStatistics() {
+    firestoreHelper.generateStatistics();
+  }
 }
 
-const timeEntryService = new TimeEntryService(
-  firestoreHelper,
-  oAuthHelper,
-);
+const timeEntryService = new TimeEntryService(firestoreHelper, oAuthHelper);
 
 export { timeEntryService };
