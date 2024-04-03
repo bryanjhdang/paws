3a2c1f515067a9a8f8a134c2d8145cc7f757ae86
stokely <stokelydiamantis@gmail.com>
2024-04-02 22:28:34 -0700

    print user id from payload, testing

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -2,10 +2,12 @@ import { Request, NextFunction, Response } from "express";
 import { AccountHelper } from "../helpers/interface/account.helper";
 import { oAuthHelper } from "../helpers/oAuth.helper";
 
+import { validateAccessToken } from "../middlewares/auth0.middleware";
 
 // todo: edit this to provide global auth
 export function authenticate(req : Request, res : Response, next : NextFunction) {
     const accountHelper : AccountHelper  = oAuthHelper;
+    validateAccessToken;
     accountHelper.getUser("nemLmP1npemf5VSzAKRC").then(user => {
         res.locals.user = user;
         next();
