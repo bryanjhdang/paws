9e9daf97b8dc97c3ef73be677f8ab94a3825dbff
stokely <stokelydiamantis@gmail.com>
2024-04-03 02:07:51 -0700

    consolidate middleware

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -1,21 +1,55 @@
+import * as dotenv from "dotenv";
 import { Request, NextFunction, Response } from "express";
 import { AccountHelper } from "../helpers/interface/account.helper";
 import { oAuthHelper } from "../helpers/oAuth.helper";
+import {
+  auth,
+  claimCheck,
+  InsufficientScopeError,
+} from "express-oauth2-jwt-bearer";
 
-export function authenticate(req : Request, res : Response, next : NextFunction) {
-    const accountHelper : AccountHelper  = oAuthHelper;
+dotenv.config();
 
-    const auth = req.auth;
-    // console.log(auth?.header);
-    console.log(auth?.payload.sub);
+export function authenticate(req: Request, res: Response, next: NextFunction) {
+  const accountHelper: AccountHelper = oAuthHelper;
 
-    if (auth?.payload.sub) {
-        accountHelper.getUser(auth.payload.sub).then(user => {
-            res.locals.user = user;
-            next();
-        }); 
-    } else {
-        return res.status(401).json({message: 'Authorization token not provided'});
-    }
-    
+  const auth = req.auth;
+  // console.log(auth?.header);
+  console.log(auth?.payload.sub);
+
+  if (auth?.payload.sub) {
+    accountHelper.getUser(auth.payload.sub).then((user) => {
+      res.locals.user = user;
+      next();
+    });
+  } else {
+    return res
+      .status(401)
+      .json({ message: "Authorization token not provided" });
+  }
 }
+
+export const validateAccessToken = auth({
+  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
+  audience: process.env.AUTH0_AUDIENCE,
+});
+
+export const checkRequiredPermissions = (requiredPermissions: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const permissionCheck = claimCheck((payload) => {
+      const permissions = payload.permissions as string[];
+
+      const hasPermissions = requiredPermissions.every((requiredPermission) =>
+        permissions.includes(requiredPermission)
+      );
+
+      if (!hasPermissions) {
+        throw new InsufficientScopeError();
+      }
+
+      return hasPermissions;
+    });
+
+    permissionCheck(req, res, next);
+  };
+};
