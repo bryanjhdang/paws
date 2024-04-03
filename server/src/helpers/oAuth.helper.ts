c01d77caa27b06bd40ca5f645c2f41efbb3945f6
stokely <stokelydiamantis@gmail.com>
2024-04-03 16:10:33 -0700

    add new user to db if not found

--- server/src/helpers/oAuth.helper.ts
+++ server/src/helpers/oAuth.helper.ts
@@ -6,6 +6,12 @@ export class OAuthHelper {
     async getUser(oAuthSub: string): Promise<User> {
         return firestoreHelper.getUser(oAuthSub);
     }
+
+    async addNewUser(oAuthSub: string): Promise<User> {
+        let newUser = new User(oAuthSub, 'default_name', new Pet(), undefined, 0);
+        const id = firestoreHelper.addUser(newUser);
+        return firestoreHelper.getUser(oAuthSub); // todo: return directly?
+    }
 }
 
 const oAuthHelper = new OAuthHelper();
