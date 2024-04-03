62e96ffaaa63bc4238a00e7ecad94d52ecf9a335
stokely <stokelydiamantis@gmail.com>
2024-04-03 02:00:26 -0700

    middleware to update user based on jwt

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -2,14 +2,20 @@ import { Request, NextFunction, Response } from "express";
 import { AccountHelper } from "../helpers/interface/account.helper";
 import { oAuthHelper } from "../helpers/oAuth.helper";
 
-import { validateAccessToken } from "../middlewares/auth0.middleware";
-
-// todo: edit this to provide global auth
 export function authenticate(req : Request, res : Response, next : NextFunction) {
     const accountHelper : AccountHelper  = oAuthHelper;
-    validateAccessToken;
-    accountHelper.getUser("nemLmP1npemf5VSzAKRC").then(user => {
-        res.locals.user = user;
-        next();
-    }); 
+
+    const auth = req.auth;
+    // console.log(auth?.header);
+    console.log(auth?.payload.sub);
+
+    if (auth?.payload.sub) {
+        accountHelper.getUser(auth.payload.sub).then(user => {
+            res.locals.user = user;
+            next();
+        }); 
+    } else {
+        return res.status(401).json({message: 'Authorization token not provided'});
+    }
+    
 }
