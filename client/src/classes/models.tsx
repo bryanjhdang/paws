280e8be7ff9becc023a738965528fecf4fc47011
Bryan <bryandang10@gmail.com>
2024-04-06 14:08:32 -0700

    get todos

--- client/src/classes/models.tsx
+++ client/src/classes/models.tsx
@@ -57,7 +57,7 @@ export class User {
 export class Todo {
   constructor(
     public task: string,
-    public done: boolean,
-    public id: string,
+    public done: boolean = false,
+    public id: string = "",
   ) { }
 }
