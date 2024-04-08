f30fee60d7d1e93c375d5b63f2ebd28b41a65618
stokely <stokelydiamantis@gmail.com>
2024-04-08 04:06:57 -0700

    rm comments

--- client/src/pages/Statistics/StatisticsPage.tsx
+++ client/src/pages/Statistics/StatisticsPage.tsx
@@ -2,36 +2,9 @@ import { useEffect, useState } from "react";
 import { Text, Flex } from "@mantine/core";
 import { TimeEntry } from "../../classes/models";
 import { getTimeEntry } from "../../classes/HTTPhelpers";
-// import { getTimeEntryTest } from "../../classes/HTTPhelpers";
-// import { useAuth0 } from "@auth0/auth0-react";
 
 function StatisticsPage() {
   const [timeEntries, setTimeEntries] = useState<TimeEntry[]>([]);
-  // const { getAccessTokenSilently } = useAuth0();
-
-  // useEffect(() => {
-  //   const fetchAccessToken = async () => {
-  //     try {
-  //       const token = await getAccessTokenSilently();
-  //       // setAccessToken(token);
-  //       getTimeEntryTest(token).then(
-  //         (response) => {
-  //           setTimeEntries(response);
-  //         }
-  //       )
-  //     } catch (error) {
-  //       console.error('Error while fetching access token:', error);
-  //     }
-  //   };
-
-  //   fetchAccessToken();
-
-  //   // getTimeEntryTest(token).then(
-  //   //   (response) => {
-  //   //     setTimeEntries(response);
-  //   //   }
-  //   // )
-  // }, []);
 
   useEffect(() => {
     getTimeEntry().then(
