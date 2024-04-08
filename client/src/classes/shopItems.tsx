1f3601cb97d08e86b4e55d6c9453dce9e170890c
Bryan <bryandang10@gmail.com>
2024-04-08 02:46:38 -0700

    add cats to store

--- client/src/classes/shopItems.tsx
+++ client/src/classes/shopItems.tsx
@@ -10,16 +10,16 @@ import WorkCat4 from '../assets/work-cats/work-cat-4.gif';
 
 // Rest Cats
 export const RestCats: CatItem[] = [
-  new CatItem(RestCat1, "Whiskers", 0),
-  new CatItem(RestCat2, "Mochi", 100),
-  new CatItem(RestCat3, "Nimbus", 200),
-  new CatItem(RestCat4, "Tofu", 300),
+  new CatItem(RestCat1, "Whiskers", 0, 1),
+  new CatItem(RestCat2, "Mochi", 100, 2),
+  new CatItem(RestCat3, "Nimbus", 200, 3),
+  new CatItem(RestCat4, "Tofu", 300, 4),
 ]
 
 //  Work Cats
 export const WorkCats: CatItem[] = [
-  new CatItem(WorkCat1, "Bubbles", 0),
-  new CatItem(WorkCat2, "Pixie", 100),
-  new CatItem(WorkCat3, "Snickers", 200),
-  new CatItem(WorkCat4, "Frisbee", 300),
+  new CatItem(WorkCat1, "Bubbles", 0, 5),
+  new CatItem(WorkCat2, "Pixie", 100, 6),
+  new CatItem(WorkCat3, "Snickers", 200, 7),
+  new CatItem(WorkCat4, "Frisbee", 300, 8),
 ]
