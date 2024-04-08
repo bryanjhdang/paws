5bf2ac4d7501f1e824d9bb6228612f04d922defd
Bryan <bryandang10@gmail.com>
2024-04-08 00:03:49 -0700

    add temp table

--- client/src/pages/Projects/ProjectsPage.tsx
+++ client/src/pages/Projects/ProjectsPage.tsx
@@ -1,10 +1,40 @@
-import { Stack } from "@mantine/core";
+import { Stack, Table } from "@mantine/core";
 import { TextHeader } from "../../components/Headers";
 
 function ProjectsPage() {
+  const elements = [
+    { position: 6, mass: 12.011, symbol: 'C', name: 'Carbon' },
+    { position: 7, mass: 14.007, symbol: 'N', name: 'Nitrogen' },
+    { position: 39, mass: 88.906, symbol: 'Y', name: 'Yttrium' },
+    { position: 56, mass: 137.33, symbol: 'Ba', name: 'Barium' },
+    { position: 58, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
+  ];
+
+  const rows = elements.map((element) => (
+    <Table.Tr key={element.name}>
+      <Table.Td>{element.position}</Table.Td>
+      <Table.Td>{element.name}</Table.Td>
+      <Table.Td>{element.symbol}</Table.Td>
+      <Table.Td>{element.mass}</Table.Td>
+    </Table.Tr>
+  ));
+
+
   return (
     <Stack>
       <TextHeader text="Projects" />
+      {/* Display projects */}
+      <Table>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>Project</Table.Th>
+            <Table.Th>Color</Table.Th>
+            <Table.Th>Date Created</Table.Th>
+            <Table.Th>Delete</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
     </Stack>
   )
 }
