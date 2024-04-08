f6594b6a61f3d3be199e03c6607e67359b127d70
Bryan <bryandang10@gmail.com>
2024-04-07 23:07:48 -0700

    store page setup

--- client/src/pages/Store/StorePage.tsx
+++ client/src/pages/Store/StorePage.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
-import { Text, Flex, Space, Stack } from "@mantine/core";
+import { Text, Flex, Space, Stack, Divider, Group } from "@mantine/core";
 import { Pet } from "../../classes/models";
 import { getCoins, getPet } from "../../classes/HTTPhelpers";
 import { TextHeader } from "../../components/Headers";
+import StoreItem from "./StoreItem";
+import classes from "./StorePage.module.css";
+import { IconCoin } from '@tabler/icons-react';
 
 function StorePage() {
   // const [storeItems, setStoreItems] = useState<>
@@ -17,21 +20,37 @@ function StorePage() {
     );
     getCoins().then(
       (response) => {
+        console.log(response);
         setCoins(response);
       }
     )
   }, []);
 
   return (
-    <>
+    <Stack>
       <TextHeader text="Store" />
-      <Text>TODO: Display coins, items, and pet icon.</Text>
-      <Text>Pet Data:</Text>
-      <Text>{JSON.stringify(petData)}</Text>
-      <Space h={30} />
-      <Text>Total Coins:</Text>
-      <Text>{JSON.stringify(coins)}</Text>
-    </>
+
+      <Group gap={5} className={classes.coinsAmount}>
+        <IconCoin stroke={1} />
+        <Text>{coins}</Text>
+      </Group>
+
+      <Flex direction={"column"} className={classes.section}>
+        <Text className={classes.sectionTitle}>Rest Cats</Text>
+        <Text>These are the cats you'll see when you're not using the timer! They're taking a break, too.</Text>
+      </Flex>
+
+      {/* Display all sleep cats */}
+
+      <StoreItem />
+
+      <Flex direction={"column"} className={classes.section}>
+        <Text className={classes.sectionTitle}>Work Cats</Text>
+        <Text className={classes.sectionText}>These cats are hard at work. Whenever the timer is on, they work alongside you!</Text>
+      </Flex>
+
+      {/* Display all work cats */}
+    </Stack>
   );
 }
 
