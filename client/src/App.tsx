3e698d902d625b7c6a64d34ddcf7436a3ccb5580
stokely <stokelydiamantis@gmail.com>
2024-04-08 03:40:15 -0700

    working localstorage impl

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,62 +14,88 @@ import { AuthenticationGuard } from "./utils/Auth0/AuthenticationGuard";
 import { CallbackPage } from "./pages/Callback/CallbackPage";
 import { NotFoundPage } from "./pages/NotFound/NotFoundPage";
 import { useEffect, useState } from "react";
-import axios from 'axios';
-import { getTimeEntryTest } from "./classes/HTTPhelpers";
+import axios from "axios";
+// import axios from 'axios';
+// import { getTimeEntryTest } from "./classes/HTTPhelpers";
+
+// const AuthContext = React.createContext( { })
 
 export const App: React.FC = () => {
+
   const { isLoading, getAccessTokenSilently } = useAuth0();
-  const [accessToken, setAccessToken] = useState<string | null>(null);
+  // const { isLoading } = useAuth0();
+  // const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [accessToken, setAccessToken] = useState(() => {
+    return localStorage.getItem('accessToken') || null;
+  });
 
-  // creating an Axios instance
-  // const axiosInstance = axios.create({
-  //   baseURL: 'https://api.timbucktoo.com',
-  // });
 
-  // Setting the default headers
+  // // creating an Axios instance
+  // // const axiosInstance = axios.create({
+  // //   baseURL: 'https://api.timbucktoo.com',
+  // // });
+
+  // // Setting the default headers
   
-  // axiosInstance.defaults.headers.post['Content-Type'] = 'application/json';
+  // // axiosInstance.defaults.headers.post['Content-Type'] = 'application/json';
+
+
 
   useEffect(() => {
     const fetchAccessToken = async () => {
       try {
         const token = await getAccessTokenSilently();
         setAccessToken(token);
+        localStorage.setItem('accessToken', token);
+
+        // if (accessToken) {
+        //   console.log("Attaching token: ", accessToken);
+        //   axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+        //   // axiosInstance.defaults.headers.common['Authorization'] = `Bearer YOUR_ACCESS_TOKEN`;
+    
+        //   // getTimeEntryTest(accessToken).then(
+        //   // (response) => {
+        //   //   console.log(response);
+        //   // })
+        // } else {
+        //   console.error("NO ACCESS TOKEN\n"); 
+        // }
+
       } catch (error) {
         console.error('Error while fetching access token:', error);
       }
     };
 
     fetchAccessToken();
-    console.log("got access token: ", accessToken);
+    // console.log("got access token: ", accessToken);
 
-    if (accessToken) {
-      console.log("Attaching token: ", accessToken);
-      axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-      // axiosInstance.defaults.headers.common['Authorization'] = `Bearer YOUR_ACCESS_TOKEN`;
+    // if (accessToken) {
+    //   console.log("Attaching token: ", accessToken);
+    //   axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+    //   // axiosInstance.defaults.headers.common['Authorization'] = `Bearer YOUR_ACCESS_TOKEN`;
 
-      // getTimeEntryTest(accessToken).then(
-      // (response) => {
-      //   console.log(response);
-      // })
-    } else {
-      console.error("NO ACCESS TOKEN\n"); 
-    }
+    //   // getTimeEntryTest(accessToken).then(
+    //   // (response) => {
+    //   //   console.log(response);
+    //   // })
+    // } else {
+    //   console.error("NO ACCESS TOKEN\n"); 
+    // }
 
 
     // axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
   }, [getAccessTokenSilently, accessToken]);
 
   // set default headers for axios requests
-  // useEffect(() => {
-  //   if (accessToken) {
-  //     console.log("Attaching token: ", accessToken);
-  //     axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-  //   } else {
-  //     console.error("NO ACCESS TOKEN\n");
+  useEffect(() => {
+    if (accessToken) {
+      console.log("Attaching token: ", accessToken);
+      axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+    } else {
+      console.error("NO ACCESS TOKEN\n");
       
-  //   }
-  // }, [accessToken]);
+    }
+  }, [accessToken]);
 
 
   if (isLoading) {
