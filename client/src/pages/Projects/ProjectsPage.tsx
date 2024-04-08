17def6d7c2bf634aa0af58f23d22b118a6bf28c5
Bryan <bryandang10@gmail.com>
2024-04-07 23:54:25 -0700

    add projects path

---
+++ client/src/pages/Projects/ProjectsPage.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "@mantine/core";
+import { TextHeader } from "../../components/Headers";
+
+function ProjectsPage() {
+  return (
+    <Stack>
+      <TextHeader text="Projects" />
+    </Stack>
+  )
+}
+
+export default ProjectsPage;
