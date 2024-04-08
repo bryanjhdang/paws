7787ff065360da4c3a170c00a05023a74a64a872
Bryan <bryandang10@gmail.com>
2024-04-08 00:49:15 -0700

    fix project storing bug

--- client/src/pages/Projects/ProjectsPage.tsx
+++ client/src/pages/Projects/ProjectsPage.tsx
@@ -1,19 +1,26 @@
 import { Button, Stack, Table } from "@mantine/core";
 import { TextHeader } from "../../components/Headers";
+import { Project } from "../../classes/models";
+import { useEffect, useState } from "react";
+import { getProjects } from "../../classes/HTTPhelpers";
 
 function ProjectsPage() {
-  const elements = [
-    { id: 6, mass: 12.011, hex: 'C', name: 'Project 1' },
-    { id: 7, mass: 14.007, hex: 'N', name: 'Project 2' },
-    { id: 39, mass: 88.906, hex: 'Y', name: 'Project 3' },
-    { id: 56, mass: 137.33, hex: 'Ba', name: 'Project 4' }
-  ];
+  const [projects, setProjects] = useState<Project[]>([]);
 
-  const rows = elements.map((element) => (
+  useEffect(() => {
+    getProjects().then(
+      (response) => {
+        setProjects(response);
+        console.log(response);
+      }
+    );
+  }, []);
+
+  const rows = projects.map((element) => (
     <Table.Tr key={element.id}>
       <Table.Td>{element.name}</Table.Td>
       <Table.Td>{element.hex}</Table.Td>
-      <Table.Td>{element.mass}</Table.Td>
+      <Table.Td>TODO</Table.Td>
       <Table.Td>
         <Button>Edit</Button>
         <Button>Delete</Button>
