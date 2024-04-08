feb087e7d02d251a96fd2fd3fbba27f6123401d4
Bryan <bryandang10@gmail.com>
2024-04-07 17:07:30 -0700

    change navbar translate to scale

--- client/src/components/NavbarSimple.tsx
+++ client/src/components/NavbarSimple.tsx
@@ -11,7 +11,10 @@ interface NavbarProps {
 const data = [
   { link: '/timer', label: 'Timer', icon: IconClock },
   { link: '/store', label: 'Store', icon: IconBuildingStore },
-  { link: '/statistics', label: 'Stats', icon: IconChartBar },
+  { link: '/statistics', label: 'Stats', icon: IconChartBar }
+];
+
+const footerData = [
   { link: '/settings', label: 'Settings', icon: IconSettings }
 ];
 
@@ -35,11 +38,32 @@ export function NavbarSimple({ active, setActive }: NavbarProps) {
     </a>
   ));
 
+  const footerLinks = footerData.map((item) => (
+    <a
+      className={classes.link}
+      data-active={item.label === active || undefined}
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.label);
+        navigate(item.link);
+      }}
+    >
+      <item.icon className={classes.linkIcon} stroke={1.5} />
+      <span>{item.label}</span>
+    </a>
+  ));
+
   return (
     <nav className={classes.navbar}>
       <div className={classes.navbarMain}>
         <Text className={classes.header}>Paws</Text>
         {links}
+      </div>
+
+      <div className={classes.footer}>
+        {footerLinks}
       </div>
     </nav>
   );
