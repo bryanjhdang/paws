0e0f69ccb88840c2c61ef32d8ee8b872ce512f60
Bryan <bryandang10@gmail.com>
2024-04-08 00:15:37 -0700

    add field names to table

--- client/src/pages/Projects/ProjectsPage.tsx
+++ client/src/pages/Projects/ProjectsPage.tsx
@@ -1,21 +1,23 @@
-import { Stack, Table } from "@mantine/core";
+import { Button, Stack, Table } from "@mantine/core";
 import { TextHeader } from "../../components/Headers";
 
 function ProjectsPage() {
   const elements = [
-    { position: 6, mass: 12.011, symbol: 'C', name: 'Carbon' },
-    { position: 7, mass: 14.007, symbol: 'N', name: 'Nitrogen' },
-    { position: 39, mass: 88.906, symbol: 'Y', name: 'Yttrium' },
-    { position: 56, mass: 137.33, symbol: 'Ba', name: 'Barium' },
-    { position: 58, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
+    { id: 6, mass: 12.011, hex: 'C', name: 'Project 1' },
+    { id: 7, mass: 14.007, hex: 'N', name: 'Project 2' },
+    { id: 39, mass: 88.906, hex: 'Y', name: 'Project 3' },
+    { id: 56, mass: 137.33, hex: 'Ba', name: 'Project 4' }
   ];
 
   const rows = elements.map((element) => (
-    <Table.Tr key={element.name}>
-      <Table.Td>{element.position}</Table.Td>
+    <Table.Tr key={element.id}>
       <Table.Td>{element.name}</Table.Td>
-      <Table.Td>{element.symbol}</Table.Td>
+      <Table.Td>{element.hex}</Table.Td>
       <Table.Td>{element.mass}</Table.Td>
+      <Table.Td>
+        <Button>Edit</Button>
+        <Button>Delete</Button>
+      </Table.Td>
     </Table.Tr>
   ));
 
@@ -30,7 +32,6 @@ function ProjectsPage() {
             <Table.Th>Project</Table.Th>
             <Table.Th>Color</Table.Th>
             <Table.Th>Date Created</Table.Th>
-            <Table.Th>Delete</Table.Th>
           </Table.Tr>
         </Table.Thead>
         <Table.Tbody>{rows}</Table.Tbody>
