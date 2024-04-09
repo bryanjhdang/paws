17e2dd674e2f4f2da2a446a3d4231d39c7aabed1
Joshua Li <jla747@sfu.ca>
2024-04-09 16:05:02 -0700

    Additoinal changes.

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,9 +21,17 @@ import { wsInit } from "./ws";
 
 dotenv.config();
 
+
+
 const app: Express = express();
 const port = process.env.PORT || 8080;
 const address = process.env.ADDRESS || "localhost"
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173"
+  }
+});
 
 app.use(cors());
 app.use(express.json());
