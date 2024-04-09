4d3ba3e4e85b4a3c9cc775656fc1a3de09017186
Bryan <bryandang10@gmail.com>
2024-04-09 11:45:56 -0700

    cleanup not found page

--- client/src/non-app-pages/NotFound/NotFoundPage.tsx
+++ client/src/non-app-pages/NotFound/NotFoundPage.tsx
@@ -1,13 +1,17 @@
-import { Flex } from "@mantine/core";
+import { Stack, Text } from "@mantine/core";
 import React from "react";
+import classes from "./NotFoundPage.module.css";
 
 // todo: flesh our this stub for unhandled routes.  home button, etc?
 export const NotFoundPage: React.FC = () => {
   return (
-    <Flex>
-      <h2>
-        Not Found
-      </h2>
-    </Flex>
+    <Stack mt={'50px'} align="center">
+      <Text className={classes.header}>
+        Sorry, this page isn't available.
+      </Text>
+      <Text>
+        The link you followed may be broken, or the page may have been removed.
+      </Text>
+    </Stack>
   );
 };
