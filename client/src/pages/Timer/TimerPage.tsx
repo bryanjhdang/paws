8c01b1d0d63d01bba4e7e627160b52161389c20c
Bryan <bryandang10@gmail.com>
2024-04-09 09:40:20 -0700

    fix typo

--- client/src/pages/Timer/TimerPage.tsx
+++ client/src/pages/Timer/TimerPage.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Flex, Image, ScrollArea, Stack } from "@mantine/core";
-import { TaskInput } from "./InputBar/TaskInput";
-import { Timer } from "./InputBar/Timer";
+import { TaskInput } from "./TaskInputBar/TaskInput";
+import { Timer } from "./TaskInputBar/Timer";
 import { Project } from "../../classes/models";
 import TodoList from "./Todo/TodoList";
 import classes from "./TimerPage.module.css";
