79132750a9f7b636d8d175ef7ac7b0a591a482a1
Bryan <bryandang10@gmail.com>
2024-04-09 16:55:17 -0700

    quick frontend styling

--- client/src/pages/Timer/TimerPage.tsx
+++ client/src/pages/Timer/TimerPage.tsx
@@ -4,14 +4,14 @@ import { TaskInput } from "./TaskInputBar/TaskInput";
 import { Timer } from "./TaskInputBar/Timer";
 import { Project } from "../../classes/models";
 import TodoList from "./Todo/TodoList";
-import SocketConnection from "./SocketConnection";
+import SocketConnection from "./SocketConnection/SocketConnection";
 // import classes from "./TimerPage.module.css";
 // import BongoCat from "../../assets/sleepy-cat-1.gif";
 
 function TimerPage() {
   const [task, setTask] = useState<string>("");
   const [selectedProject, setSelectedProject] = useState<Project | null>(null);
-  
+
   return (
     <Flex direction={"row"} py={20} px={40} gap={20}>
       <Flex direction={"column"} flex={1}>
@@ -27,8 +27,10 @@ function TimerPage() {
           selectedProject={selectedProject}
         />
       </Flex>
-      <TodoList />
-      <SocketConnection></SocketConnection>
+      <Flex direction={"column"} gap={20}>
+        <TodoList />
+        <SocketConnection />
+      </Flex>
     </Flex>
   );
 }
