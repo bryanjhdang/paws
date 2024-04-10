a48a9d3f0c6446cf8467d4a543f94754de723861
Bryan <bryandang10@gmail.com>
2024-04-09 20:44:38 -0700

    remove modal from timer button

--- client/src/pages/Timer/ProjectButton.tsx
+++ client/src/pages/Timer/ProjectButton.tsx
@@ -5,54 +5,9 @@ import { useEffect, useState } from "react";
 import { Project } from "../../classes/models";
 import { getProjects, postProject } from "../../classes/HTTPhelpers";
 // import classes from "./ProjectButton.module.css";
-// import NewProjectModal from "../../components/NewProjectModal";
+import NewProjectModal from "../../components/NewProjectModal";
 import { useAuth0 } from "@auth0/auth0-react";
 // import classes from "./ProjectButton.module.css";
-
-interface NewProjectModalProps {
-  opened: boolean;
-  close: () => void,
-  onAddProject: (project: Project) => void;
-}
-
-function NewProjectModal({ opened, close, onAddProject }: NewProjectModalProps): JSX.Element {
-
-  const [projectName, setProjectName] = useState("");
-  const [projectColor, setProjectColor] = useState(DEFAULT_THEME.colors.red[4]);
-
-  const handleCreateProject = () => {
-    const newProject = new Project(projectColor, projectName, Date.now(), Date.now().toString());
-    onAddProject(newProject);
-    close();
-    setProjectName("");
-    setProjectColor(DEFAULT_THEME.colors.red[4]);
-  }
-
-  return (
-    <Modal opened={opened} onClose={close} title="Create new project" centered>
-      <Stack>
-        <TextInput
-          withAsterisk
-          placeholder={"Project name"}
-          value={projectName}
-          onChange={(event) => setProjectName(event.currentTarget.value)}
-        />
-        <ColorPicker
-          format="hex"
-          value={projectColor}
-          onChange={(color) => setProjectColor(color)}
-          withPicker={false}
-          fullWidth
-          swatches={
-            Object.values(DEFAULT_THEME.colors).map(colorArray => colorArray[4])
-          }
-        />
-        <Text>{projectColor}</Text>
-        <Button w={"100%"} onClick={handleCreateProject}>Create project</Button>
-      </Stack>
-    </Modal>
-  )
-}
 
 interface ProjectButtonProps {
   selectedProject: Project | null;
