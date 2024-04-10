b0e1f10370c8ff6763b6908f9f899cb98078cd92
yenu <35312312+yenuGH@users.noreply.github.com>
2024-04-09 21:17:28 -0700

    seperate label to seperate custom function

--- client/src/pages/Timer/SocketConnection/SocketConnection.tsx
+++ client/src/pages/Timer/SocketConnection/SocketConnection.tsx
@@ -1,7 +1,17 @@
-import { Accordion, ActionIcon, Button, Stack, Text } from "@mantine/core"
+import {
+  Accordion,
+  ActionIcon,
+  Button,
+  Group,
+  Stack,
+  Text,
+} from "@mantine/core";
 import { useContext, useEffect, useState } from "react";
-import { Socket, io } from "socket.io-client"
-import { SocketContext, useSocketContext } from "../../../context/SocketContext";
+import { Socket, io } from "socket.io-client";
+import {
+  SocketContext,
+  useSocketContext,
+} from "../../../context/SocketContext";
 import { IconLink } from "@tabler/icons-react";
 import classes from "./SocketConnection.module.css";
 import { User } from "../../../classes/models";
@@ -19,24 +29,37 @@ function Connection({ connection }: ConnectionProp) {
   )
 } */
 
+interface UserLabelProps {
+  user: User;
+}
+function UserLabel({ user }: UserLabelProps) {
+  return (
+    <Group wrap="nowrap">
+      <Text>{user.displayName}</Text>
+    </Group>
+  );
+}
+
 interface UserListProps {
-    users: User[]
+  users: User[];
 }
 function UserList({ users }: UserListProps) {
-    const items = users.map((user) => (
-        <Accordion.Item key={user.id} value={user.displayName}>
-          <Accordion.Control >{user.displayName}</Accordion.Control>
-          <Accordion.Panel>{user.runningTime.name}</Accordion.Panel>
-        </Accordion.Item>
-      ));
+  const items = users.map((user) => (
+    <Accordion.Item key={user.id} value={user.displayName}>
+      <Accordion.Control>
+        <UserLabel user={user} />
+      </Accordion.Control>
+      <Accordion.Panel>{user.runningTime.name}</Accordion.Panel>
+    </Accordion.Item>
+  ));
 
-    return (
-        <>
-            <Accordion variant="filled" radius={"md"}>
-                {items}
-            </Accordion>
-        </>
-    );
+  return (
+    <>
+      <Accordion variant="filled" radius={"md"}>
+        {items}
+      </Accordion>
+    </>
+  );
 }
 
 function SocketConnection() {
@@ -50,11 +73,11 @@ function SocketConnection() {
       setUsers(data.filter((e) => e.id != user?.sub));
     }
 
-    socket.on('users', updateUsers);
+    socket.on("users", updateUsers);
 
     return () => {
-      socket.off('users'); 
-    }
+      socket.off("users");
+    };
   }, []);
 
   function socketConnect() {
@@ -64,7 +87,7 @@ function SocketConnection() {
       return;
     }
 
-    socket.emit("join")
+    socket.emit("join");
     setJoined(true);
   }
 
@@ -80,7 +103,7 @@ function SocketConnection() {
       </Button>
       <UserList users={users} />
     </Stack>
-  )
+  );
 }
 
-export default SocketConnection
+export default SocketConnection;
