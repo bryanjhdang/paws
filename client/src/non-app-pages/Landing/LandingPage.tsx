b8f945ad50d43aca890ef3122217add29f88d156
Bryan <bryandang10@gmail.com>
2024-04-09 20:07:21 -0700

    create landing page

--- client/src/non-app-pages/Landing/LandingPage.tsx
+++ client/src/non-app-pages/Landing/LandingPage.tsx
@@ -1,111 +1,95 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Center, Box, Flex, Text, Title, Button } from "@mantine/core";
+import { Text, Title, Button, Container } from "@mantine/core";
+import { IconBrandGithub } from "@tabler/icons-react";
 import { useEffect } from "react";
+import classes from './HeroText.module.css';
 
-function LandingPage() {
-  const { loginWithRedirect, isAuthenticated } = useAuth0();   
+// function LandingPage() {
+//   const { loginWithRedirect, isAuthenticated } = useAuth0();   
+//   const navigate = useNavigate();
+
+//     // todo: some flicker in transition from Landing to Timer, may be inherent to current styling
+//     useEffect(() => {
+//         if (isAuthenticated) {
+//             navigate('/timer');
+//         } 
+//     }, [isAuthenticated, navigate]);
+
+//     const handleLogin = async () => {
+//         await loginWithRedirect({
+//             appState: {
+//                 returnTo: "/timer",
+//             },
+//             authorizationParams: {
+//                 prompt: "login",
+//             },
+//         });
+//     };
+
+
+
+
+export default function LandingPage() {
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
   const navigate = useNavigate();
-  
-    // todo: some flicker in transition from Landing to Timer, may be inherent to current styling
-    useEffect(() => {
-        if (isAuthenticated) {
-            navigate('/timer');
-        } 
-    }, [isAuthenticated, navigate]);
-    
-    const handleLogin = async () => {
-        await loginWithRedirect({
-            appState: {
-                returnTo: "/timer",
-            },
-            authorizationParams: {
-                prompt: "login",
-            },
-        });
-    };
 
-    function signInButton() {
-        return (
-            <>
-                <Button
-                    onClick={handleLogin}
-                    variant="gradient"
-                    gradient={{
-                        from: 'yellow',
-                        to: 'orange',
-                        deg: 90
-                    }}
-                >
-                    SIGN IN
-                </Button>
-            </>
-        );
+  // todo: some flicker in transition from Landing to Timer, may be inherent to current styling
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/timer');
     }
+  }, [isAuthenticated, navigate]);
 
-    // function templifyTitle() {
-    //     return (
-    //         <>
-    //             <Center>
-    //                 <Title
-    //                     order={1}
-    //                     size={100}
-    //                 >
-    //                     <Text
-    //                         inherit
-    //                         span
-    //                         fw={900}
-    //                         variant='gradient'
-    //                         gradient={{
-    //                             from: 'rgba(255, 157, 71, 1)',
-    //                             to: 'rgba(252, 210, 96, 1)',
-    //                             deg: 90
-    //                         }}
-    //                     >
-    //                         TEMPLIFY
-    //                     </Text>
-    //                 </Title>
-    //             </Center>
-    //         </>
-    //     );
-    // }
-
-  function pawsTitle() {
-    return (
-      <>
-        <Center>
-          <Title order={1} size={100}>
-            <Text
-              inherit
-              span
-              fw={900}
-              variant="gradient"
-              gradient={{
-                from: "rgba(255, 157, 71, 1)",
-                to: "rgba(252, 210, 96, 1)",
-                deg: 90,
-              }}
-            >
-              PAWS
-            </Text>
-          </Title>
-        </Center>
-      </>
-    );
-  }
+  const handleLogin = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: "/timer",
+      },
+      authorizationParams: {
+        prompt: "login",
+      },
+    });
+  };
 
   return (
-    <>
-      <Center style={{ height: "100vh" }}>
-        <Box style={{ width: 200 }}>
-          <Flex direction="column" gap="md">
-            {pawsTitle()}
-            {signInButton()}
-          </Flex>
-        </Box>
-      </Center>
-    </>
+    <Container className={classes.wrapper} size={1400}>
+      <div className={classes.inner}>
+        <Title className={classes.title}>
+          Gamified{' '}
+          <Text component="span" className={classes.highlight} inherit>
+            time tracking
+          </Text>{' '}
+          for all your needs. With cats.
+        </Title>
+
+        <Container p={0} size={600}>
+          <Text size="lg" c="dimmed" className={classes.description}>
+            Use a simple and efficient time tracker that you'll love alongside your feline friends. Do some work, earn coins, and track your every move.
+          </Text>
+        </Container>
+
+        <div className={classes.controls}>
+          <Button 
+            className={classes.control} 
+            size="lg" 
+            variant="default" 
+            color="gray"
+            leftSection={<IconBrandGithub size={20} />}
+            onClick={() => window.open('https://github.com/bryanjhdang/tempify', '_blank')}
+          >
+            GitHub
+          </Button>
+          <Button 
+            onClick={handleLogin} 
+            className={classes.control} 
+            size="lg" 
+            color="#764b61"
+          >
+            Get started
+          </Button>
+        </div>
+      </div>
+    </Container>
   );
 }
-
-export default LandingPage;
