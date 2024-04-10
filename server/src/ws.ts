99ec6aa3a3c41042728920be2370a5789dff0012
Joshua Li <jla747@sfu.ca>
2024-04-09 20:42:47 -0700

    This works I think?

--- server/src/ws.ts
+++ server/src/ws.ts
@@ -1,14 +1,35 @@
-import { User } from "./models/User";
+import { NoRunning, RunningCountdown, User } from "./models/User";
 import { Socket } from "socket.io";
 import { accountService } from "./services/account.service";
 
+interface JoinedUser {
+    displayName: string, 
+    petIconId: number, 
+    id: string, 
+    startTime: number, 
+    entryName: string
+
+}
 
 export function wsInit(io: any) {
     const connectedUsers: Map<string, User> = new Map<string, User>();
 
+    function simplifyUser(user : User) : JoinedUser {
+        let isWorking = user.runningTime instanceof NoRunning;
+        return {
+            displayName: user.displayName,
+            petIconId: isWorking ? user.pet.workId : user.pet.restId,
+            id: user.id,
+            startTime: user.runningTime instanceof RunningCountdown ? user.runningTime.startTime : 0,
+            entryName: user.runningTime instanceof RunningCountdown ? user.runningTime.name : "poo poo time"
+        }
+    }
+
     function updateUsers(user: User) {
         connectedUsers.set(user.id, user);
-        io.emit(`users`, [...connectedUsers.values()]);
+        let t =  [...connectedUsers.values()];
+        console.log(t);
+        io.emit(`users`, t);
     }
 
     io.on("connection", (socket: Socket) => {
@@ -33,16 +54,8 @@ export function wsInit(io: any) {
         });
 
         socket.on('updateUser', (user: User) => {
+            console.log(user);
             updateUsers(user);
         });
-
-        socket.on("leave", async () => {
-            let userId = socket.handshake.query.token;
-            if (!userId) { return }
-
-
-            connectedUsers.delete(userId.toString());
-            io.emit("users", [...connectedUsers.values()]);
-        })
     });
 }
