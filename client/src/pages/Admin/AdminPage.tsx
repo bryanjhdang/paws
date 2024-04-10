e6a9779e6198032c71289ed82493445ad7826cc6
stokely <stokelydiamantis@gmail.com>
2024-04-09 19:53:31 -0700

    update endpoints to add coins

--- client/src/pages/Admin/AdminPage.tsx
+++ client/src/pages/Admin/AdminPage.tsx
@@ -2,10 +2,11 @@ import { Stack, Text, Button } from "@mantine/core";
 import { SimpleHeader } from "../../components/Headers";
 import { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { addCoins } from "../../classes/HTTPhelpers";
 
 function AdminPage() {
   const [isAdmin, setIsAdmin] = useState(false);
-  const { getIdTokenClaims } = useAuth0();
+  const { user, getIdTokenClaims, getAccessTokenSilently } = useAuth0();
 
   useEffect(() => {
     getIdTokenClaims().then((token) => {
@@ -19,6 +20,17 @@ function AdminPage() {
     })
   }, [getIdTokenClaims]);
 
+  const handleAddCoins = (numCoins: number) => {
+    getAccessTokenSilently().then((token) => {
+      // const userId = user?.sub || "not logged in";
+      addCoins(numCoins, token).catch(error => {
+        console.error("Could not add coins:", error);
+      });
+    }).catch(error => {
+      console.error(error);
+    })
+  };
+
   return (
     <>
       <SimpleHeader text="Admin" />
@@ -26,7 +38,7 @@ function AdminPage() {
         /* Admin content */
         <Stack p={40} align="flex-start">
           <Text>{"authorized!"}</Text>
-          <Button variant="filled" size="md">Add Coins</Button>
+          <Button variant="filled" size="md" onClick={() => handleAddCoins(100)}>Add Coins</Button>
         </Stack>
       ) : (
         /* Non-Admin content */
