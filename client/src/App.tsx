8c9341a7680a9c21397b41473c2d147fc4d17f4c
stokely <stokelydiamantis@gmail.com>
2024-04-09 18:31:10 -0700

    add admin page and display to 'Admin' role

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import LandingPage from "./non-app-pages/Landing/LandingPage";
 import TimerPage from "./pages/Timer/TimerPage";
 import StorePage from "./pages/Store/StorePage";
 import StatisticsPage from "./pages/Statistics/StatisticsPage";
+import AdminPage from "./pages/Admin/AdminPage";
 import { useAuth0 } from "@auth0/auth0-react";
 import { PageLoader } from "./components/PageLoader";
 import { AuthenticationGuard } from "./utils/Auth0/AuthenticationGuard";
@@ -58,6 +59,14 @@ export const App: React.FC = () => {
         element={<AuthenticationGuard 
           component={() => (
             <BasePage pageName="Projects"><ProjectsPage /></BasePage>
+          )}
+        />}
+      />
+      <Route
+        path="/admin"
+        element={<AuthenticationGuard 
+          component={() => (
+            <BasePage pageName="Admin"><AdminPage /></BasePage>
           )}
         />}
       />
---
+++ client/src/pages/Admin/AdminPage.tsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+import { Stack } from "@mantine/core";
+import { SimpleHeader } from "../../components/Headers";
+// import { useAuth0 } from "@auth0/auth0-react";
+// import classes from "./SettingsPage.module.css";
+
+function AdminPage() {
+  return (
+    <>
+      <SimpleHeader text="Admin" />
+      <Stack p={40} flex={1}>
+      </Stack>
+    </>
+  )
+}
+
+export default AdminPage
